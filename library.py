1f614414cb081ab13c23f9f5983a10c3c599ff97
aakshaysugan <aakshaysugan@gmail.com>
2026-01-08 20:57:12 +0530

    Initial commit

---
+++ library.py
@@ -0,0 +1,14 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def display_books(self):
+        for book in self.books:
+            print(book)
+
+library = Library()
+library.add_book("Python")
+library.display_books()
